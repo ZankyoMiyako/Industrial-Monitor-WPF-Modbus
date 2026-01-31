963d219dd7bbdd9c64c3224a06fdcad309dd6804
ZankyoMiyako <ZankyoMiyako@outlook.com>
2026-01-31 22:36:46 +0800

    下载配置prism框架与material design

---
+++ App.xaml.cs
@@ -0,0 +1,24 @@
+﻿using Industrial_Monitor_WPF_Modbus.Views;
+using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Industrial_Monitor_WPF_Modbus
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : PrismApplication
+    {
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            
+        }
+    }
+
+}
