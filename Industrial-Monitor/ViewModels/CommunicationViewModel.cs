10b8b892cd5b3d950e7a298abac60fa2a9712bf1
ZankyoMiyako <ZankyoMiyako@outlook.com>
2026-02-03 17:54:37 +0800

    feat(ui):通过事件聚合器,实现了跨Viewmodel通信,让通信设置按钮能够打开右侧边栏进行详细设置

--- Industrial-Monitor/ViewModels/CommunicationViewModel.cs
+++ Industrial-Monitor/ViewModels/CommunicationViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Industrial_Monitor.Core.Events;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,17 @@ namespace Industrial_Monitor.ViewModels
 {
     internal class CommunicationViewModel
     {
+        public CommunicationViewModel(IEventAggregator eventAggregator)
+        {
+            aggregator = eventAggregator;
+            OpenDrawerCommand = new DelegateCommand(() => aggregator.GetEvent<DrawerControlEvent>().Publish(true));
+        }
+        #region 事件聚合器引用
+        private IEventAggregator aggregator;
+        #endregion
+        #region 右侧边栏发布命令
+        public DelegateCommand OpenDrawerCommand { get; }
+        #endregion
+
     }
 }
--- Industrial-Monitor/ViewModels/MainWindowViewModel.cs
+++ Industrial-Monitor/ViewModels/MainWindowViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Industrial_Monitor.Core.Events;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,28 @@ using System.Threading.Tasks;
 
 namespace Industrial_Monitor.ViewModels
 {
-    class MainWindowViewModel
+    class MainWindowViewModel : BindableBase
     {
+        public MainWindowViewModel(IEventAggregator eventaggregator)
+        {
+            aggregator=eventaggregator;
+            aggregator.GetEvent<DrawerControlEvent>().Subscribe(IsOpen => IsRightDrawerOpen = IsOpen,ThreadOption.UIThread);
+        }
+        #region 事件聚合器引用
+        private readonly IEventAggregator aggregator;
+        #endregion
+        #region 右侧边栏的开关属性
+        private bool _IsRightDrawerOpen;
+
+        public bool IsRightDrawerOpen
+        {
+            get { return _IsRightDrawerOpen; }
+            set
+            {
+                _IsRightDrawerOpen = value;
+                RaisePropertyChanged();
+            }
+        }
+        #endregion
     }
 }
