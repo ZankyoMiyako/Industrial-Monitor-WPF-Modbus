9a753d78b327bf83a8b547faa20c84cfc1cbbccf
ZankyoMiyako <ZankyoMiyako@outlook.com>
2026-02-03 17:49:11 +0800

    refactor(architecture):创建类库并引用,创建了控制右侧边栏的事件类

---
+++ Industrial-Monitor.Core/Events/DrawerControlEvent.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Industrial_Monitor.Core.Events
+{
+    public class DrawerControlEvent : PubSubEvent<bool> { }
+}
