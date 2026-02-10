43a8ca9d6cf782b842634900155fa3112ba2d84c
ZankyoMiyako <ZankyoMiyako@outlook.com>
2026-02-10 17:36:30 +0800

    feat(service):创建并简单实现了IModbusMasterService接口

---
+++ Industrial-Monitor.Core/Services/IModbusMasterService.cs
@@ -0,0 +1,20 @@
+﻿using Industrial_Monitor.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Industrial_Monitor.Core.Services
+{
+    public interface IModbusMasterService
+    {
+        //连接参数
+        ConnectionConfigParameters ConnectionParameters { get; set; }
+        //连接状态
+        bool IsConnected { get;}
+        //连接方法
+        bool Connect(ConnectionConfigParameters Parameters);
+    }
+}
---
+++ Industrial-Monitor.Core/Services/ModbusMasterService.cs
@@ -0,0 +1,58 @@
+﻿using Industrial_Monitor.Core.Models;
+using NModbus;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Industrial_Monitor.Core.Services
+{
+    public class ModbusMasterService : IModbusMasterService
+    {
+        //tcp客户端
+        private TcpClient _tcpClient;
+        //主站
+        private IModbusMaster _master;
+        //工厂
+        private IModbusFactory _factory;
+        //重连次数
+        private int _currentReconnectAttempts;
+        //连接参数
+        public ConnectionConfigParameters ConnectionParameters { get; set; }
+        //连接状态
+        public bool IsConnected { get; private set; }
+        public ModbusMasterService()
+        {
+            _factory=new ModbusFactory();
+        }
+        //连接方法
+        public bool Connect(ConnectionConfigParameters Parameters)
+        {
+            ConnectionParameters = Parameters;
+            if (IsConnected)
+                return true;
+            try
+            {
+                _tcpClient = new TcpClient();
+                _tcpClient.Connect(Parameters.IpAddress, Parameters.Port);
+                _master = _factory.CreateMaster(_tcpClient);
+                IsConnected = true;
+                _currentReconnectAttempts = 0;
+                Debug.WriteLine("连接成功");
+                return true;
+            }
+            catch(Exception ex)
+            {
+                IsConnected = false;
+                _master = null;
+                _tcpClient?.Close();
+                _tcpClient = null;
+                Debug.WriteLine("连接失败");
+                return false;
+            }
+        }
+    }
+}
