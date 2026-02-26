ec00eb7d21b5e71883743baa7bc864530eaeea1f
ZankyoMiyako <ZankyoMiyako@outlook.com>
2026-02-26 16:17:21 +0800

    feat(modbus):实现了通过modbus tcp轮询获取数据并显示在控件上

--- Industrial-Monitor/ViewModels/ModbusDataViewModel.cs
+++ Industrial-Monitor/ViewModels/ModbusDataViewModel.cs
@@ -10,15 +10,18 @@ using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace Industrial_Monitor.ViewModels
 {
-    internal class ModbusDataViewModel:BindableBase
+    internal class ModbusDataViewModel : BindableBase
     {
         //事件聚合器引用
-         private readonly IEventAggregator _aggregator;
+        private readonly IEventAggregator _aggregator;
         //引用IModbusMasterService服务
-        private readonly IModbusMasterService _masterService;
+        private readonly IModbusMasterService _modbusMaster;
+        //
+        private readonly IRequestConfigService _requestConfigService;
         //数据集合,可用于绑定
         private ObservableCollection<ModbusDataItem> _ModbusDataItems;
         public ObservableCollection<ModbusDataItem> ModbusDataItems
@@ -26,17 +29,24 @@ namespace Industrial_Monitor.ViewModels
             get { return _ModbusDataItems; }
             set { SetProperty(ref _ModbusDataItems, value); }
         }
-        public ModbusDataViewModel(IEventAggregator aggregator,IModbusMasterService masterService)
+        public ModbusDataViewModel(IEventAggregator aggregator, IModbusMasterService masterService, IRequestConfigService requestConfigService)
         {
-            _aggregator=aggregator;
-            _masterService=masterService;
-            ModbusDataItems=new ObservableCollection<ModbusDataItem>();
-            _aggregator.GetEvent<ModbusDataItemGeneratedEvent>().Subscribe(OnRequestConfigUpdated);
+            _aggregator = aggregator;
+            _modbusMaster = masterService;
+            _requestConfigService = requestConfigService;
+            ModbusDataItems = new ObservableCollection<ModbusDataItem>();
+            _aggregator.GetEvent<ModbusDataItemGeneratedEvent>().Subscribe(OnRequestConfigUpdated,ThreadOption.UIThread,true);
         }
         private void OnRequestConfigUpdated(ObservableCollection<ModbusDataItem> collection)
         {
-            ModbusDataItems=collection;
-            throw new NotImplementedException();
+            ModbusDataItems = collection;
+            _modbusMaster.StartPolling(_requestConfigService.SlaveId, _requestConfigService.FunctionCode, ModbusDataItems, 500, (reg, value) =>
+            {
+                Application.Current.Dispatcher.Invoke(() =>
+                {
+                    reg.Value = value;
+                });
+            });
         }
     }
 }
